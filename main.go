54e58060b7a9822f77bf4dc5f2858b5a85dc6ec5
NkVn <pandsandkrecker29@gmail.com>
2025-02-01 15:13:16 +0300

    Fix return value for post handler

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"encoding/json"
-	"fmt"
 	"net/http"
 
 	"github.com/gorilla/mux"
@@ -22,7 +21,7 @@ func PostHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	DB.Create(&request)
-	fmt.Fprintln(w, "Task added successfully")
+	json.NewEncoder(w).Encode(request)
 }
 
 func main() {
