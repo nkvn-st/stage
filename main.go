71011da132b9ad309db911abb1730d29bce0a279
NkVn <pandsandkrecker29@gmail.com>
2025-02-05 17:18:25 +0300

    Add patch and delete handlers

--- main.go
+++ main.go
@@ -3,25 +3,57 @@ package main
 import (
 	"encoding/json"
 	"net/http"
+	"strconv"
 
 	"github.com/gorilla/mux"
 )
 
 func GetHandler(w http.ResponseWriter, r *http.Request) {
-	var tasks []Message
-	DB.Find(&tasks)
-	json.NewEncoder(w).Encode(tasks)
+	var messages []Message
+	DB.Find(&messages)
+	json.NewEncoder(w).Encode(messages)
 }
 
 func PostHandler(w http.ResponseWriter, r *http.Request) {
-	var request Message
-	if err := json.NewDecoder(r.Body).Decode(&request); err != nil {
+	var message Message
+	if err := json.NewDecoder(r.Body).Decode(&message); err != nil {
 		http.Error(w, "Bad request", http.StatusBadRequest)
 		return
 	}
 
-	DB.Create(&request)
-	json.NewEncoder(w).Encode(request)
+	DB.Create(&message)
+	json.NewEncoder(w).Encode(message)
+}
+
+func PatchHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid id", http.StatusBadRequest)
+		return
+	}
+
+	var message Message
+	if err := json.NewDecoder(r.Body).Decode(&message); err != nil {
+		http.Error(w, "Bad request", http.StatusBadRequest)
+		return
+	}
+
+	DB.Model(&Message{}).Where("id = ?", id).Update("is_done", message.IsDone)
+	DB.Find(&message, id)
+	json.NewEncoder(w).Encode(message)
+}
+
+func DeleteHandler(w http.ResponseWriter, r *http.Request) {
+	idStr := mux.Vars(r)["id"]
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		http.Error(w, "Invalid id", http.StatusBadRequest)
+		return
+	}
+
+	DB.Unscoped().Delete(&Message{}, id)
+	json.NewEncoder(w).Encode(map[string]string{"message": "Message was deleted"})
 }
 
 func main() {
@@ -32,5 +64,7 @@ func main() {
 	router := mux.NewRouter()
 	router.HandleFunc("/api/get", GetHandler).Methods("GET")
 	router.HandleFunc("/api/post", PostHandler).Methods("POST")
+	router.HandleFunc("/api/patch/{id}", PatchHandler).Methods("PATCH")
+	router.HandleFunc("/api/delete/{id}", DeleteHandler).Methods("DELETE")
 	http.ListenAndServe(":8080", router)
 }
